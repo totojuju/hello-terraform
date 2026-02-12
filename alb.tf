c33c4d146a342e93b085bffe3275f6827411aced
yuta <yuta323clan@gmail.com>
2026-02-12 21:18:17 +0900

    SSLポリシーを既存ALBのものに合わせる

--- alb.tf
+++ alb.tf
@@ -51,7 +51,7 @@ resource "aws_lb_listener" "https" {
     load_balancer_arn = aws_lb.hello.arn
     port = 443
     protocol = "HTTPS"
-    ssl_policy = "ELBSecurityPolicy-2016-08"
+    ssl_policy = "ELBSecurityPolicy-TLS13-1-2-Res-PQ-2025-09"
     certificate_arn = aws_acm_certificate_validation.hello.certificate_arn
 
     default_action {
